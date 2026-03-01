46809e4e09a5aba4820142ad6d78929e69ef5d8e
frigoutsos <franklin_rigoutsos@brown.edu>
2026-03-01 12:53:16 -0500

    feature: added navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,23 @@
 import Link from "next/link";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import Image from "next/image";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  variable: "--font-plus-jakarta-sans",
+})
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link href="/">Home</Link> |{" "}
-      <Link href="/about">About</Link> |{" "}
-      <Link href="/delegations">Delegations</Link> |{" "}
-      <Link href="/apply">Apply</Link> |{" "}
-      <Link href="/events">Events</Link> |{" "}
-      <Link href="/contact">Contact</Link> |{" "}
-      <a href="#">Donate</a>
+    <nav className={`${plusJakartaSans.className} flex items-center space-x-4 justify-between text-[#333333] bg-[#fdf6f6] px-8 py-4`}>
+      <Image src="/logo.svg" width={100} height={100} alt="Youth Japan Logo" className="w-10 h-10" />
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/delegations">Delegations</Link>
+      <Link href="/apply">Apply</Link>
+      <Link href="/events">Events</Link>
+      <Link href="/contact">Contact</Link>
+      <Link className="bg-[#AA302C] text-white rounded-tr-lg rounded-md p-1 whitespace-nowrap" href="#">Donate Now</Link>
     </nav>
   );
 }
