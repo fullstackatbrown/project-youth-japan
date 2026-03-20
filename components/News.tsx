240022cb01923226a03893ea64d62da1a6602cf1
ianchang1 <131323440+ianchang1@users.noreply.github.com>
2026-03-20 16:07:21 -0400

    Added news Sections (#11)

---
+++ components/News.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+
+const News = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold text-white md:text-4xl lg:text-5xl bg-[#B91F24] py-5 text-center rounded-[40px] max-w-292  mx-auto">
+        News
+      </h1>
+      <div className="grid grid-cols-3 gap-12 pt-10 px-10">
+        <div className="mb-2">
+          <div className="bg-[#D9D9D9] h-75 mb-2"></div>
+          <p className="text-lg">30 february 2028</p>
+          <h1 className="text-3xl font-bold my-3 text-[#003366]">
+            LOREM IPSUM LOREM IPSUM LOREM
+          </h1>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur. Ut duis enim donec odio leo
+            nam hendrerit ullamcorper elementum. Laoreet interdum mattis varius
+            varius condimentum elit adipiscing sed non. Libero ipsum vitae
+            pellent
+          </p>
+          <h2 className="text-xl text-[#B91F24] mt-2">READ MORE ➜ </h2>
+        </div>
+        <div className="mb-2">
+          <div className="bg-[#D9D9D9] h-75 mb-2"></div>
+          <p className="text-lg">30 february 2028</p>
+          <h1 className="text-3xl font-bold my-3 text-[#003366]">
+            LOREM IPSUM LOREM IPSUM LOREM
+          </h1>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur. Ut duis enim donec odio leo
+            nam hendrerit ullamcorper elementum. Laoreet interdum mattis varius
+            varius condimentum elit adipiscing sed non. Libero ipsum vitae
+            pellent
+          </p>
+          <h2 className="text-xl text-[#B91F24] mt-2">READ MORE ➜ </h2>
+        </div>
+        <div className="mb-2">
+          <div className="bg-[#D9D9D9] h-75 mb-2"></div>
+          <p className="text-lg">30 february 2028</p>
+          <h1 className="text-3xl font-bold my-3 text-[#003366]">
+            LOREM IPSUM LOREM IPSUM LOREM
+          </h1>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur. Ut duis enim donec odio leo
+            nam hendrerit ullamcorper elementum. Laoreet interdum mattis varius
+            varius condimentum elit adipiscing sed non. Libero ipsum vitae
+            pellent
+          </p>
+          <h2 className="text-xl text-[#B91F24] mt-2">READ MORE ➜ </h2>
+        </div>
+        <div className="mb-2">
+          <div className="bg-[#D9D9D9] h-75 mb-2"></div>
+          <p className="text-lg">30 february 2028</p>
+          <h1 className="text-3xl font-bold my-3 text-[#003366]">
+            LOREM IPSUM LOREM IPSUM LOREM
+          </h1>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur. Ut duis enim donec odio leo
+            nam hendrerit ullamcorper elementum. Laoreet interdum mattis varius
+            varius condimentum elit adipiscing sed non. Libero ipsum vitae
+            pellent
+          </p>
+          <h2 className="text-xl text-[#B91F24] mt-2">READ MORE ➜ </h2>
+        </div>
+        <div className="mb-2">
+          <div className="bg-[#D9D9D9] h-75 mb-2"></div>
+          <p className="text-lg">30 february 2028</p>
+          <h1 className="text-3xl font-bold my-3 text-[#003366]">
+            LOREM IPSUM LOREM IPSUM LOREM
+          </h1>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur. Ut duis enim donec odio leo
+            nam hendrerit ullamcorper elementum. Laoreet interdum mattis varius
+            varius condimentum elit adipiscing sed non. Libero ipsum vitae
+            pellent
+          </p>
+          <h2 className="text-xl text-[#B91F24] mt-2">READ MORE ➜ </h2>
+        </div>
+        <div className="mb-2">
+          <div className="bg-[#D9D9D9] h-75 mb-2"></div>
+          <p className="text-lg">30 february 2028</p>
+          <h1 className="text-3xl font-bold my-3 text-[#003366]">
+            LOREM IPSUM LOREM IPSUM LOREM
+          </h1>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur. Ut duis enim donec odio leo
+            nam hendrerit ullamcorper elementum. Laoreet interdum mattis varius
+            varius condimentum elit adipiscing sed non. Libero ipsum vitae
+            pellent
+          </p>
+          <h2 className="text-xl text-[#B91F24] mt-2">READ MORE ➜ </h2>
+        </div>
+      </div>
+      <div className="h-64"></div>
+    </div>
+  );
+};
+
+export default News;
