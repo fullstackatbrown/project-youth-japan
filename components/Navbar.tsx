02bd95079f2b4008fce1a92ba1741210439307f9
Cornelia Crumley <corneliacrumley@Cornelias-MacBook-Pro-2079.local>
2026-04-30 11:38:28 -0400

    Adding ja translations

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,38 +4,48 @@ import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import LanguageSwitcher from "./LanguageSwitcher";
-import { Plus_Jakarta_Sans } from "next/font/google";
-
-const plusJakartaSans = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  variable: "--font-plus-jakarta-sans",
-});
+import { useTranslations } from "next-intl";
 
 const links = [
-  { href: '/', label: 'Home' },
-  { href: '/about', label: 'About' },
-  { href: '/members', label: 'Members' },
-  { href: '/apply', label: 'Apply' },
-  { href: '/events', label: 'Events' },
-  { href: '/library', label: 'Library' },
-  { href: '/contact', label: 'Contact Us' },
+  { href: "", labelKey: "home" },
+  { href: "/about", labelKey: "about" },
+  { href: "/members", labelKey: "members" },
+  { href: "/apply", labelKey: "apply" },
+  { href: "/events", labelKey: "events" },
+  { href: "/library", labelKey: "library" },
+  { href: "/contact", labelKey: "contact" },
 ];
 
 export default function Navbar() {
   const pathname = usePathname();
   const locale = pathname.split("/")[1] || "en";
+  const t = useTranslations("Navbar");
 
   return (
-    <nav className={`${plusJakartaSans.className} flex items-center space-x-4 justify-between text-[#333333] bg-[#fdf6f6] px-8 py-4`}>
-      <Image src="/logo.svg" width={100} height={100} alt="Youth Japan Logo" className="w-10 h-10" />
-      <Link href={`/${locale}`}>Home</Link>
-      <Link href={`/${locale}/about`}>About</Link>
-      <Link href={`/${locale}/members`}>Members</Link>
-      <Link href={`/${locale}/apply`}>Apply</Link>
-      <Link href={`/${locale}/events`}>Events</Link>
-      <Link href={`/${locale}/library`}>Library</Link>
-      <Link href={`/${locale}/contact`}>Contact Us</Link>
-      <Link className="bg-[#AA302C] text-white rounded-tr-lg rounded-md p-1 whitespace-nowrap" href="#">Donate Now</Link>
+    <nav className="flex items-center space-x-4 justify-between text-[#333333] bg-[#fdf6f6] px-8 py-4">
+      <Link href={`/${locale}`}>
+        <Image
+          src="/logo.svg"
+          width={100}
+          height={100}
+          alt="Youth Japan Logo"
+          className="w-10 h-10"
+        />
+      </Link>
+
+      {links.map((link) => (
+        <Link key={link.labelKey} href={`/${locale}${link.href}`}>
+          {t(link.labelKey)}
+        </Link>
+      ))}
+
+      <Link
+        className="bg-[#AA302C] text-white rounded-tr-lg rounded-md p-1 whitespace-nowrap"
+        href="#"
+      >
+        {t("donate")}
+      </Link>
+
       <LanguageSwitcher />
     </nav>
   );
